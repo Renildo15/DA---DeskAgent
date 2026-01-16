499859e82c17e54c5bfccea773c5878165fa3de4
Renildo15 <renildorabi@ufrn.edu.br>
2026-01-16 19:50:28 -0300

    config: configurando pwa

--- deskagent_app/src/routes/+layout.svelte
+++ deskagent_app/src/routes/+layout.svelte
@@ -1,9 +1,19 @@
 <script lang="ts">
 	import './layout.css';
 	import favicon from '$lib/assets/favicon.svg';
+	import { onMount } from 'svelte';
 
 	let { children } = $props();
+
+	onMount(() => {
+		if ('serviceWorker' in navigator) {
+			navigator.serviceWorker.register('/service-worker.js');
+		}
+	});
+
+	
 </script>
 
 <svelte:head><link rel="icon" href={favicon} /></svelte:head>
 {@render children()}
+
