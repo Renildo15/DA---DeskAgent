3bb28c9e298ebdb981b7585fcd6ccd4f996fcae4
Renildo15 <renildorabi@ufrn.edu.br>
2026-01-16 20:20:05 -0300

    fix||:hosts

--- deskagent_api/deskagent_api/settings.py
+++ deskagent_api/deskagent_api/settings.py
@@ -28,7 +28,12 @@ SECRET_KEY = config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["https://deskagent-api.onrender.com"]
+ALLOWED_HOSTS = [
+    "deskagent-api.onrender.com",
+    "localhost",
+    "127.0.0.1",
+]
+
 
 
 # Application definition
