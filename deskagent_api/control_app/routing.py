3ba729fd98e3b3c54f0b9d99f1364f336f27d2ae
Renildo15 <renildorabi@ufrn.edu.br>
2026-01-27 18:46:49 -0300

    fix: corrigindso routing

--- deskagent_api/control_app/routing.py
+++ deskagent_api/control_app/routing.py
@@ -1,6 +1,8 @@
-from django.urls import re_path
-from .consumers import ControlConsumer
+from django.urls import re_path, path
+from .consumers import ControlConsumer, PCInfoConsumer
 
 websocket_urlpatterns = [
     re_path(r"ws/control/$", ControlConsumer.as_asgi()),
+    re_path(r"ws/pc_info/$", PCInfoConsumer.as_asgi()),
+    path("", ControlConsumer.as_asgi()),
 ]
